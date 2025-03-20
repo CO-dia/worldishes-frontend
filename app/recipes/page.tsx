6a39f76b00a22fd30000b337c22ab3c95271ea14
Dia Raboana <rdiadama@gmail.com>
2025-03-20 01:38:34 -0400

    Added console log to debug

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -4,7 +4,7 @@ import { CardRecipe } from "@/components/CardRecipe";
 // Fetch recipes on the server for SEO
 const getRecipes = async (): Promise<Array<Dish>> => {
   const res = await fetch(
-    `${process.env.API_URL}${process.env.API_VERSION}dishes`
+    `${process.env.API_URL}${process.env.API_VERSION}dishes`,
   );
 
   return res.json();
@@ -12,12 +12,14 @@ const getRecipes = async (): Promise<Array<Dish>> => {
 
 export default async function Page() {
   const recipes = await getRecipes();
+  console.log({ recipes });
 
   return (
     <>
       <h1>Recipes</h1>
       <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6">
-        {recipes && recipes.length > 0 &&
+        {recipes &&
+          recipes.length > 0 &&
           recipes.map((recipe) => (
             <CardRecipe recipe={recipe} key={recipe.id} />
           ))}
